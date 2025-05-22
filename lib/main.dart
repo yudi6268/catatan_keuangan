2a892fa8120547f7952e4d11e355d5bd0a856f84
Wahyudi <yudi6268@gmail.com>
2025-05-22 22:46:52 +0700

    konek supabase html

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
+    await Supabase.initialize(
     url: 'https://prsbsxqddrfdwyxntgfn.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByc2JzeHFkZHJmZHd5eG50Z2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwOTI4NzUsImV4cCI6MjA2MjY2ODg3NX0.FCdb0mN02TfNzD30bihChlnD9l_EVArGIuwAAX3FjAE',
   );
   runApp(const MyApp());
 }
-//test test test
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
